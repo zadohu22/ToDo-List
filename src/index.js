8d3f040df0a54831668c94d3a329555f3f921412
zadohu <zadohu22@gmail.com>
2022-07-08 11:54:07 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+class ToDo{
+    constructor(title, list, notes, priority, date, time){
+        this.title = title;
+        this.list = list;
+        this.notes = notes;
+        this.priority = priority;
+        this.date = date;
+        this.time = time;
+    }
+
+    
+}
