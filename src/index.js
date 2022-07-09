a1144364f1cc361ff6a3f4a25ed7b49518822b74
zadohu <zadohu22@gmail.com>
2022-07-09 16:05:03 -0500

    refactor js

--- src/index.js
+++ src/index.js
@@ -2,20 +2,18 @@ import { ToDo } from "./create";
 import SimplePicker from "simplepicker";
 
 let userInput;
+let createButton = document.getElementById("addNewToDo");
+let popup = document.getElementById("createToDoPopup");
+let overlay = document.getElementById("blackOverlay");
+let createButtonPopup = document.getElementById("submitPopup");
+let popupInput = document.getElementById("userInput");
+let dateButton = document.getElementById("dateButton");
+let pickerDiv = document.getElementById("pickerDiv");
+let dueDate = document.getElementById("dueDate");
+let textArea = document.getElementById("textArea");
+let radios = document.querySelectorAll('input[type="radio"]');
 
-const createButtonListeners = (() => {
-  let createButton = document.getElementById("addNewToDo");
-  let popup = document.getElementById("createToDoPopup");
-  let overlay = document.getElementById("blackOverlay");
-  let createButtonPopup = document.getElementById("submitPopup");
-  let popupInput = document.getElementById("userInput");
-  let dateButton = document.getElementById("dateButton");
-  let pickerDiv = document.getElementById("pickerDiv");
-  let dueDate = document.getElementById("dueDate");
-  let textArea = document.getElementById("textArea");
-  let radios = document.querySelectorAll('input[type="radio"]');
-
-  createButton.addEventListener("click", () => {
+createButton.addEventListener("click", () => {
     popup.style.display = "flex";
     overlay.style.display = "block";
     popupInput.value = "";
@@ -27,20 +25,20 @@ const createButtonListeners = (() => {
     }))
   });
 
-  createButtonPopup.addEventListener("click", () => {
+createButtonPopup.addEventListener("click", () => {
     popup.style.display = "none";
     overlay.style.display = "none";
     userInput = popupInput.value;
   });
 
-  overlay.addEventListener("click", () => {
+overlay.addEventListener("click", () => {
     popup.style.display = "none";
     overlay.style.display = "none";
     pickerDiv.style.display = "none";
     popupInput.value = "";
   });
 
-  dateButton.addEventListener("click", () => {
+dateButton.addEventListener("click", () => {
     pickerDiv.style.display = "block";
     let picker = new SimplePicker(pickerDiv, {
       zIndex: 10,
@@ -50,8 +48,5 @@ const createButtonListeners = (() => {
       dueDate.innerHTML = `Due: ${readableDate}`;
     })
   });
-
-  
-})();
 
 
