b4f27a1e2af8bb4f61761c460af949a5ef7eac00
zadohu <zadohu22@gmail.com>
2022-07-19 22:25:06 -0500

    realizes that new Todos should be an array of Objects.

--- src/create.js
+++ src/create.js
@@ -1,9 +1,10 @@
 class ToDo{
-    constructor(title, date, time, priority, notes){
+    constructor(title, date, time, priority, number, notes){
         this.title = title;
         // this.list = list;
         this.notes = notes;
         this.priority = priority;
+        this.number = number;
         this.date = date;
         this.time = time;
     }
--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ let radios = document.querySelectorAll('input[type="radio"]');
 let content = document.getElementById("content");
 let cardContainer = document.getElementById('cardContainer');
 let checkbox = document.getElementById("taskNameCheck");
+let i = 0;
 
 createButton.addEventListener("click", () => {
     popup.style.display = "flex";
@@ -27,6 +28,11 @@ createButton.addEventListener("click", () => {
       radio.checked = false;
     }))
   });
+
+// *********** BEGIN ADD TASK LISTENER ************** //
+// *********** BEGIN ADD TASK LISTENER ************** //
+// *********** BEGIN ADD TASK LISTENER ************** //
+
 
 addTask.addEventListener("click", () => {
   if(userInput.value != ""){
@@ -44,58 +50,87 @@ addTask.addEventListener("click", () => {
       popup.style.display = "none";
       overlay.style.display = "none";
       pickerDiv.style.display = "none";
+
+
   
+      // **** increment (i) for use in checkbox label logic, and remove logic *****
+      i++;
+      console.log(i)
+
+
+
       // ****** format usable date/time ******
       let dueDateArr = dueDate.innerHTML.split(" ");
       let usableDate = `${dueDateArr[2]} ${dueDateArr[1]}`;
       let usableTime = `${dueDateArr[4]} ${dueDateArr[5]}`;
+
+
   
       // *** create TODO object ***
-      let createTodo = new ToDo(userInput.value, usableDate, usableTime, prioritySelection());
+      let createTodo = new ToDo(userInput.value, usableDate, usableTime, prioritySelection(), `${i}`);
+      console.log(createTodo)
+
   
       // *** create DOM things ***
       let cardContainer = document.createElement('div');
       cardContainer.id="cardContainer";
       cardContainer.classList.add("cardContainer");
       content.appendChild(cardContainer);
+
+
   
       // ***** Top Of Container *****
       let topOfContainer = document.createElement('div');
       topOfContainer.classList.add("topOfContainer");
       cardContainer.appendChild(topOfContainer);
+
       
+
+      // ************  CheckBox  ******************//
+
       let taskNameCheck = document.createElement('input');
       taskNameCheck.setAttribute("type", "checkbox");
       taskNameCheck.setAttribute("name", "taskNameCheck");
-      taskNameCheck.id = "taskNameCheck";
+      taskNameCheck.id = `taskNameCheck${i}`;
       topOfContainer.appendChild(taskNameCheck);
+
+
       
-      let taskName = document.createElement("p");
-      taskName.classList.add("taskName");
-      taskName.id = "taskName";
-      taskName.innerHTML = createTodo.title;
-      topOfContainer.appendChild(taskName);
+      // ************* Task Name ******************* //
+      let taskNameCheckLabel = document.createElement("label");
+      // taskNameCheckLabel.setAttribute("for", `taskNameCheck${i}`);
+      taskNameCheckLabel.classList.add("taskName");
+      taskNameCheckLabel.id = "taskName";
+      taskNameCheckLabel.innerHTML = createTodo.title;
+      topOfContainer.appendChild(taskNameCheckLabel);
+
+
   
-      // ***** Bottom Of Container *****
+      // ***** Bottom Of Container ***** //
       let bottomOfContainer = document.createElement("div");
       bottomOfContainer.classList.add("bottomOfContainer");
       cardContainer.appendChild(bottomOfContainer);
+
+
       
-      
-      let taskPriority = document.createElement("p");
-      taskPriority.classList.add("taskPriority");
-      taskPriority.id = "taskPriority";
-      taskPriority.innerHTML = `Priority: ${createTodo.priority}`;
-      bottomOfContainer.appendChild(taskPriority);
-  
-      if(createTodo.priority == "Low"){
-        taskPriority.style.color = "green";
-      }else if(createTodo.priority == "Medium"){
-        taskPriority.style.color = "orange";
-      }else{
-        taskPriority.style.color = "red";
+      // ************* Priority Text && Color Logic *************** //
+      if (createTodo.priority != undefined){
+        let taskPriority = document.createElement("p");
+        taskPriority.classList.add("taskPriority");
+        taskPriority.id = "taskPriority";
+        taskPriority.innerHTML = `Priority: ${createTodo.priority}`;
+        bottomOfContainer.appendChild(taskPriority);
+    
+        if(createTodo.priority == "Low"){
+          taskPriority.style.color = "green";
+        }else if(createTodo.priority == "Medium"){
+          taskPriority.style.color = "orange";
+        }else{
+          taskPriority.style.color = "red";
+        }
       }
-  
+
+    // *************** Display Formatted Due Date ****************** //
   
     let taskDueDate = document.createElement("p");
     taskDueDate.classList.add("taskDueDate");
@@ -107,8 +142,14 @@ addTask.addEventListener("click", () => {
     }
   }
  
-  
 });
+
+ // ************** END ADD TASK LISTENER **************** //
+  // ************** END ADD TASK LISTENER **************** //
+   // ************** END ADD TASK LISTENER **************** //
+
+
+
 
 overlay.addEventListener("click", () => {
     popup.style.display = "none";
